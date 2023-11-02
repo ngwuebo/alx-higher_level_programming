8ba0a6e780774749bacd03b9d7cccf5e400c3345
ngwuebo <nnathaniel155@gmail.com>
2023-11-02 04:51:58 +0000

    work

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author -Godswill Kalu
+# Author -ngwuebo
 
 def pow(a, b):
     return (a ** b)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - ngwuebo
 
 def islower(c):
     """Function checks for lowercase characters."""
